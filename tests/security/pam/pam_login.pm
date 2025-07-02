836b8732d3d14a165cbcb0e91ef03973defa44b6
Ferenc Szekely <feri.szekely@suse.com>
2025-07-02 12:54:31 +0200

    changes for SLE>=16
    Signed-off-by: Ferenc Szekely <feri.szekely@suse.com>

--- tests/security/pam/pam_login.pm
+++ tests/security/pam/pam_login.pm
@@ -15,6 +15,12 @@ use version_utils;
 sub run {
     select_serial_terminal;
 
+    # pam_ssh is not part of SLE16 and above
+    if (is_sle('>=16')) {
+        record_info('SKIPPING TEST', "Skipping pam_login for now as pam_ssh.so is no longer part of SLE>=16. Refactoring might be needed.");
+        return;
+    }
+
     # Define the user and password, which are already configured in previous milestone
     my $user = 'suse';
     my $passwd = 'susetesting';
